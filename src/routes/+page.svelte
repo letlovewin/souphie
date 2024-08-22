2ddfbccea6839b212a0bc536e70d199ae7bc89c9
gray <graythecolor@proton.me>
2024-08-21 17:06:59 -0700

    ok

---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<svelte:head>
+    <title>souphie</title>
+</svelte:head>
+
+<div class="d-flex align-items-center vh-100">
+    <div class="container main-container">
+        <div class="row mt-5">
+            <div class="col d-flex align-items-center justify-content-center">
+                <img
+                    class="my-auto rounded mt-5"
+                    src={"/souphie.png"}
+                    alt="Souphie logo"
+                    style="height:330px;max-width:330px;"
+                />
+            </div>
+            <div class="col my-auto">
+                <h1><strong>souphie</strong></h1>
+                <p>
+                    this is a tutoring group with a focus on mathematics and computer science.
+                </p>
+                <p>
+                    currently we offer
+                </p>
+                <ul>
+                    <li>algebra I & II</li>
+                    <li>geometry & trigonometry</li>
+                    <li>precalculus</li>
+                    <li>calculus I & II</li>
+                    <li>statistics</li>
+                    <li>discrete mathematics</li>
+                    <li>data structures & algorithms</li>
+                </ul>
+                <p>at a rate of $25/hour. to make an appointment please DM us on <a href="https://instagram.com/souphietutoring">instagram</a>.</p>
+            </div>
+            
+        </div>
+        <div class="row">
+            <div class="text-center pt-2">
+                <a href="mailto:souphie-tutoring@proton.me">email</a> / <a href="https://instagram.com/souphietutoring">instagram</a>
+            </div>
+        </div>
+    </div>
+</div>
